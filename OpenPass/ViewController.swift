d3f132706efa530d15b93a3ff2ee54f14369ec26
Sylvan M <alephnulltim@gmail.com>
2018-12-30 21:38:11 -0500

    Initial Commit

---
+++ OpenPass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenPass
+//
+//  Created by Sylvan Martin on 12/30/18.
+//  Copyright © 2018 Sylvan Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
