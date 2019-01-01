efd67575ea3abfd17173b9e71f094bf564dbfb1c
Sylvan M <alephnulltim@gmail.com>
2019-01-01 18:37:11 -0500

    Added "shouldEncrypt" property, as well as made other properties binary data

--- OpenPass/Data Management/Account+CoreDataClass.swift
+++ OpenPass/Data Management/Account+CoreDataClass.swift
@@ -2,14 +2,15 @@
 //  Account+CoreDataClass.swift
 //  OpenPass
 //
-//  Created by Sylvan Martin on 12/31/18.
-//  Copyright © 2018 Sylvan Martin. All rights reserved.
+//  Created by Sylvan Martin on 1/1/19.
+//  Copyright © 2019 Sylvan Martin. All rights reserved.
 //
 //
 
 import Foundation
 import CoreData
 
+
 public class Account: NSManagedObject {
-    
+
 }
--- OpenPass/Data Management/Account+CoreDataProperties.swift
+++ OpenPass/Data Management/Account+CoreDataProperties.swift
@@ -2,13 +2,14 @@
 //  Account+CoreDataProperties.swift
 //  OpenPass
 //
-//  Created by Sylvan Martin on 12/31/18.
-//  Copyright © 2018 Sylvan Martin. All rights reserved.
+//  Created by Sylvan Martin on 1/1/19.
+//  Copyright © 2019 Sylvan Martin. All rights reserved.
 //
 //
 
 import Foundation
 import CoreData
+
 
 extension Account {
 
@@ -16,9 +17,10 @@ extension Account {
         return NSFetchRequest<Account>(entityName: "Account")
     }
 
-    @NSManaged public var email: String?
+    @NSManaged public var email: NSData?
     @NSManaged public var name: String?
-    @NSManaged public var password: String?
-    @NSManaged public var username: String?
+    @NSManaged public var password: NSData?
+    @NSManaged public var username: NSData?
+    @NSManaged public var shouldEncrypt: Bool
 
 }
