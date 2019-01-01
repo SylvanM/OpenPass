0e7bd21e39afed5c6fad5d57e249a75472377715
Sylvan M <alephnulltim@gmail.com>
2019-01-01 18:40:52 -0500

    Added struct to easily create keys

---
+++ OpenPass/Encryption and Decryption/Keys/KeyHelper.swift
@@ -0,0 +1,36 @@
+//
+//  KeyHelper.swift
+//  OpenPass
+//
+//  Created by Sylvan Martin on 1/1/19.
+//  Copyright © 2019 Sylvan Martin. All rights reserved.
+//
+
+import Foundation
+import Security
+import CoreFoundation
+
+struct KeyHelper {
+    
+    func generateKey(for tag: Data) -> SecKey? {
+        
+        let tag = "com.OpenNav.keys.deviceKeys".data(using: .utf8)
+        
+        let attributes: [String : Any] = [
+            kSecAttrKeyType as String:            kSecAttrKeyTypeRSA,
+            kSecAttrKeySizeInBits as String:      2048,
+            kSecPrivateKeyAttrs as String: [
+                kSecAttrIsPermanent as String:    true,
+                kSecAttrApplicationTag as String: tag as Any
+            ]
+        ]
+        
+        var error: Unmanaged<CFError>?
+        
+        let privateKey = SecKeyCreateRandomKey(attributes as CFDictionary, &error)
+        print("Error on generating: ", error)
+        return privateKey
+        
+    }
+    
+}
